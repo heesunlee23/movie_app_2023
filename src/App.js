2b3a0d79444a9b7654498b5497214a3f3f7b9c72
rosacomputer <jl8867@rit.edu>
2023-08-01 09:53:23 +0900

    04-2까지 하지만 실핼 되는 건지 검증해 봐야 함

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React from 'react';
 
-function Food({ fav }) {
-  return <h1>I like { fav }</h1>;
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} />
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    // 아이스바인
+    name: 'Eisbein',
+    image: 'https://georgies.co.za/wp-content/uploads/2022/05/Eisbein-Main-image-2-cropped.jpg', 
+  }, 
+  {
+    // 슈바인스학세 
+    name: 'Schweinshaxe', 
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Schweinshaxe_Chiang_Mai.JPG/534px-Schweinshaxe_Chiang_Mai.JPG',
+  }, 
+  {
+    // 커리부어스트
+    name: 'Currywurst', 
+    image: 'https://i0.wp.com/pretzelandme.blog/wp-content/uploads/2022/06/Currywurst.jpg?w=1000&ssl=1',
+  }, 
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello, Incoming CS MS Students!</h1>
-      <Food fav="gimbap"/>
-      <Food fav="ramen"/>
-      <Food fav="samgiopsal"/>
-      <Food fav="chukumi"/>
+      {foodILike.map(dish => (<Food name={dish.name} picture={dish.image} />))}
     </div>
   );
 }
